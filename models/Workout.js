744cde97266bac6cfa50e11382dee1903fc6e414
pablito <pabloivanjuarez@gmail.com>
2020-10-31 14:19:33 -0700

    basic code added, looking for more examples in act 14 week 17

---
+++ models/Workout.js
@@ -0,0 +1,16 @@
+const {
+  Schema
+} = require("mongoose")
+
+const mongoose = require("mongoose");
+// read into act 14
+Const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date
+  },
+  Exercies: {
+    type
+  }
+})
