b0a5d0c8c151c656c9610e5fde94431bcb8c6ed0
pablito <pabloivanjuarez@gmail.com>
2020-11-06 14:57:52 -0800

    schema created for workout table

--- models/Workout.js
+++ models/Workout.js
@@ -4,13 +4,38 @@ const {
 
 const mongoose = require("mongoose");
 // read into act 14
-Const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: {
     type: Date
   },
-  Exercies: {
-    type
-  }
+  exercises: [{
+    type: {
+      type: String,
+      trim: true
+    },
+    name: {
+      type: String,
+      trim: true
+    },
+    duration: {
+      type: Number
+    },
+    weight: {
+      type: Number
+    },
+    reps: {
+      type: Number
+    },
+    sets: {
+      type: Number
+    },
+    distance: {
+      type: Number
+    }
+  }]
 })
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout
