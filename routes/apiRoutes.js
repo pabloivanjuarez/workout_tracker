0bdf00535d09adc275fc10f0044aef19da97e486
pablito <pabloivanjuarez@gmail.com>
2020-11-06 14:58:56 -0800

    post method added

---
+++ routes/apiRoutes.js
@@ -0,0 +1,21 @@
+const Workout = require('../models');
+const router = require('express').Router();
+
+router.post('/api/workouts', (req, res) => {
+  Workout.create({})
+    .then(response => {
+      res.json(response);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put('/api/workouts/:id', ({
+  body,
+  parameters
+}, res) => {
+  Workout.findByIdAndUpdate(
+    req.
+  )
+})
