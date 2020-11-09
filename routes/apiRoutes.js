22cef67169d14550072d8970743c57801dc306a6
pablito <pabloivanjuarez@gmail.com>
2020-11-09 14:38:40 -0800

    Workout path updated

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,26 @@
-const Workout = require('../models');
+const Workout = require('../models/Workout');
 const router = require('express').Router();
+
+router.get('/api/workouts/', (req, res) => {
+  Workout.find({})
+    .sort({
+      _id: -1
+    })
+    .limit(1).then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+  Workout.find({}).then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
 
 router.post('/api/workouts', (req, res) => {
   Workout.create({})
@@ -28,27 +49,6 @@ router.put('/api/workouts/:id', ({
   }).catch((err) => {
     res.json(err);
   });
-});
-
-router.get('/api/workouts/range', (req, res) => {
-  Workout.find({}).then((data) => {
-    res.json(data);
-  }).catch((err) => {
-    res.json(err);
-  });
-});
-
-router.get('/api/workouts/', (req, res) => {
-  Workout.find({})
-    .sort({
-      _id: -1
-    })
-    .limit(1).then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
 });
 
 module.exports = router;
